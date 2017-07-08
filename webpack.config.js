d9d16046bf9d1c1960c701d8b7fa74fc5fca4a66
Clark Grubb <clarkgrubb@gmail.com>
2017-07-08 07:31:44 -0700

    use typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 const path = require('path');
 
 const config = {
-    entry: {
-        sample: './src/js/sample.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'static/js/'),
-        filename: '[name].bundle.js'
-    }
+  entry: {
+    sample: './src/js/sample.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dest/js/'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: "awesome-typescript-loader" },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  }
 };
 
 module.exports = config;
